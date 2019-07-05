9369d64328a58c63a2062badfb85c37b0bb7b283
Marijan3062 <brailo.marijan@gmail.com>
2019-07-05 12:26:39 +0200

    added dropdown menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,75 +1,82 @@
-import React from "react";
-import axios from "axios";
-import Flight from "./Flight";
-import { DateTime } from "luxon";
-import Gif from "../img/spinner.gif";
+import React from 'react';
+import axios from 'axios';
+import Flight from './Flight';
+import { DateTime } from 'luxon';
+import Gif from '../img/spinner.gif';
+import Dropmenu from './Dropdown';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      flights: [],
-      isLoading: true
-    };
-  }
-
-  fetchFlights = async () => {
-    try {
-      let response = await axios.get("https://api.skypicker.com/flights", {
-        params: {
-          fly_from: "PRG",
-          fly_to: "VLC",
-          date_from: "06/07/2019",
-          date_to: "13/07/2019",
-          limit: 15
-        }
-      });
-      let { data } = response.data;
-      await this.setState({ flights: data });
-      this.setState({ isLoading: false });
-    } catch (error) {
-      console.error(error);
+    constructor(props) {
+        super(props);
+        this.state = {
+            flights: [],
+            isLoading: true
+        };
     }
-  };
 
-  componentDidMount() {
-    this.fetchFlights();
-  }
+    fetchFlights = async () => {
+        try {
+            let response = await axios.get(
+                'https://api.skypicker.com/flights',
+                {
+                    params: {
+                        fly_from: 'PRG',
+                        fly_to: 'VLC',
+                        date_from: '06/07/2019',
+                        date_to: '13/07/2019',
+                        limit: 15
+                    }
+                }
+            );
+            let { data } = response.data;
+            await this.setState({ flights: data });
+            this.setState({ isLoading: false });
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  render() {
-    const flightComponents = this.state.flights.map((flight, index) => {
-      const depTime = DateTime.fromMillis(flight.dTime * 1000).toFormat(
-        "hh:mm"
-      );
-      const arrTime = DateTime.fromMillis(flight.aTime * 1000).toFormat(
-        "hh:mm"
-      );
-      return (
-        <Flight
-          key={`flight-${index}`}
-          cityFrom={flight.cityFrom}
-          cityTo={flight.cityTo}
-          flyFrom={flight.flyFrom}
-          flyTo={flight.flyTo}
-          price={flight.price}
-          dTime={depTime}
-          aTime={arrTime}
-        />
-      );
-    });
+    componentDidMount() {
+        this.fetchFlights();
+    }
 
-    return (
-      <div className="App">
-        <div className="app-wrapper">
-          <div>
-            <h1>Kiwi Flights</h1>
-            {this.state.isLoading && <img src={Gif} alt="spinner" />}
-          </div>
-          <div>{flightComponents}</div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const flightComponents = this.state.flights.map((flight, index) => {
+            const depTime = DateTime.fromMillis(flight.dTime * 1000).toFormat(
+                'hh:mm'
+            );
+            const arrTime = DateTime.fromMillis(flight.aTime * 1000).toFormat(
+                'hh:mm'
+            );
+            return (
+                <Flight
+                    key={`flight-${index}`}
+                    cityFrom={flight.cityFrom}
+                    cityTo={flight.cityTo}
+                    flyFrom={flight.flyFrom}
+                    flyTo={flight.flyTo}
+                    price={flight.price}
+                    dTime={depTime}
+                    aTime={arrTime}
+                />
+            );
+        });
+
+        return (
+            <div className="App">
+                <div className="app-wrapper">
+                    <div>
+                        <h1>Kiwi Flights</h1>
+                        <Dropmenu />
+                        {this.state.isLoading && (
+                            <img src={Gif} alt="spinner" />
+                        )}
+                    </div>
+                    <div>{flightComponents}</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Dropdown.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Menu, Dropdown, Icon } from 'antd';
+
+const menu = (
+    <Menu>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="#">
+                Valencia
+            </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="#">
+                Barcelona
+            </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="#">
+                Madrid
+            </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="#">
+                Milano
+            </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="#">
+                Athens
+            </a>
+        </Menu.Item>
+    </Menu>
+);
+const Dropmenu = () => (
+    <Dropdown overlay={menu}>
+        <a className="ant-dropdown-link" href="#">
+            Hover me <Icon type="down" />
+        </a>
+    </Dropdown>
+);
+
+export default Dropmenu;
