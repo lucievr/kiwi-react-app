a33e3d8fe29934e9a5e414b3ca6c0ff757c35bdf
Marijan3062 <brailo.marijan@gmail.com>
2019-07-05 15:11:56 +0200

    added direct flight option

--- src/components/App.js
+++ src/components/App.js
@@ -1,135 +1,170 @@
-import React from "react";
-import axios from "axios";
-import Flight from "./Flight";
-import { DateTime } from "luxon";
-import Gif from "../img/spinner.gif";
+import React from 'react';
+import axios from 'axios';
+import Flight from './Flight';
+import { DateTime } from 'luxon';
+import Gif from '../img/spinner.gif';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      flights: [],
-      isLoading: true,
-      destinations: {
-        Valencia: "VLC",
-        Barcelona: "BCN",
-        Milano: "MXP",
-        Madrid: "MAD",
-        Athens: "ATH"
-      },
-      origins: {
-        Prague: "PRG",
-        Berlin: "SXF",
-        Warsaw: "WMI",
-        Pardubice: "PED"
-      },
-      flyFrom: '',
-      flyTo: ''
-    };
-  }
-
-  fetchFlights = async () => {
-    const { flyFrom, flyTo } = this.state
-    this.setState({isLoading: true})
-    try {
-      let response = await axios.get("https://api.skypicker.com/flights", {
-        params: {
-          fly_from: flyFrom,
-          fly_to: flyTo,
-          date_from: "06/07/2019",
-          date_to: "13/07/2019",
-          limit: 15
-        }
-      });
-      console.log(response.data)
-      this.setState({ flights: response.data.data, isLoading: false });
-    } catch (error) {
-      console.error(error);
+    constructor(props) {
+        super(props);
+        this.state = {
+            flights: [],
+            isLoading: true,
+            isDirect: false,
+            destinations: {
+                Valencia: 'VLC',
+                Barcelona: 'BCN',
+                Milano: 'MXP',
+                Madrid: 'MAD',
+                Athens: 'ATH'
+            },
+            origins: {
+                Prague: 'PRG',
+                Berlin: 'SXF',
+                Warsaw: 'WMI',
+                Pardubice: 'PED'
+            },
+            flyFrom: '',
+            flyTo: ''
+        };
     }
-  };
+    handleInputChange = event => {
+        const target = event.target;
 
-  componentDidMount() {
-    this.fetchFlights();
-  }
+        const name = target.name;
 
-  handleOriginSelect = (e) => {
-      this.setState({ flyFrom: e.target.value })
-  }
+        this.setState({
+            [name]: target.checked
+        });
+    };
 
-  handleDestSelect = (e) => {
-    this.setState({ flyTo: e.target.value })
-}
+    fetchFlights = async () => {
+        const { flyFrom, flyTo, isDirect } = this.state;
+        this.setState({ isLoading: true });
+        try {
+            let response = await axios.get(
+                'https://api.skypicker.com/flights',
+                {
+                    params: {
+                        fly_from: flyFrom,
+                        fly_to: flyTo,
+                        date_from: '06/07/2019',
+                        date_to: '13/07/2019',
+                        limit: 15,
+                        direct_flights: isDirect
+                    }
+                }
+            );
+            console.log(response.data);
+            this.setState({ flights: response.data.data, isLoading: false });
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-handleSubmit = (e) => {
-    e.preventDefault();
-    this.fetchFlights()
-}
+    componentDidMount() {
+        this.fetchFlights();
+    }
 
-  render() {
-    const flightComponents = this.state.flights.map((flight, index) => {
-      const depTime = DateTime.fromMillis(flight.dTime * 1000).toFormat(
-        "hh:mm"
-      );
-      const arrTime = DateTime.fromMillis(flight.aTime * 1000).toFormat(
-        "hh:mm"
-      );
-      return (
-        <Flight
-          key={`flight-${index}`}
-          cityFrom={flight.cityFrom}
-          cityTo={flight.cityTo}
-          flyFrom={flight.flyFrom}
-          flyTo={flight.flyTo}
-          price={flight.price}
-          dTime={depTime}
-          aTime={arrTime}
-        />
-      );
-    });
+    handleOriginSelect = e => {
+        this.setState({ flyFrom: e.target.value });
+    };
 
-    const destinationCityArray = Object.keys(this.state.destinations);
-    const originsCityArray = Object.keys(this.state.origins);
+    handleDestSelect = e => {
+        this.setState({ flyTo: e.target.value });
+    };
 
-    const destArray = destinationCityArray.map((city, index) => (
-      <option value={this.state.destinations[city]} key={`${city}-${index}`}>
-        {city}
-      </option>
-    ));
+    handleSubmit = e => {
+        e.preventDefault();
+        this.fetchFlights();
+    };
 
-    const originArray = originsCityArray.map((city, index) => (
-      <option value={this.state.origins[city]} key={`${city}-${index}`}>
-        {city}
-      </option>
-    ));
+    render() {
+        const flightComponents = this.state.flights.map((flight, index) => {
+            const depTime = DateTime.fromMillis(flight.dTime * 1000).toFormat(
+                'hh:mm'
+            );
+            const arrTime = DateTime.fromMillis(flight.aTime * 1000).toFormat(
+                'hh:mm'
+            );
+            return (
+                <Flight
+                    key={`flight-${index}`}
+                    cityFrom={flight.cityFrom}
+                    cityTo={flight.cityTo}
+                    flyFrom={flight.flyFrom}
+                    flyTo={flight.flyTo}
+                    price={flight.price}
+                    dTime={depTime}
+                    aTime={arrTime}
+                />
+            );
+        });
 
-    return (
-      <div className="App">
-        <div className="app-wrapper">
-          <div>
-            <h1>Search for flights</h1>
-            {this.state.isLoading && <img src={Gif} alt="spinner" />}
-          </div>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="origin">From</label> <br />
-            <select id="origin" onChange={this.handleOriginSelect}>
-            {originArray}
-            </select> <br />
+        const destinationCityArray = Object.keys(this.state.destinations);
+        const originsCityArray = Object.keys(this.state.origins);
 
-            <label htmlFor="destination">To</label> <br />
-            <select id="destination" onChange={this.handleDestSelect}>{destArray}</select>
-            <button onClick={this.fetchFlights}>Search</button>
-          </form>
-          <div
-            style={{
-              marginTop: `200px`
-            }}
-          >
-            {flightComponents}
-          </div>
-        </div>
-      </div>
-    );
-  }
+        const destArray = destinationCityArray.map((city, index) => (
+            <option
+                value={this.state.destinations[city]}
+                key={`${city}-${index}`}
+            >
+                {city}
+            </option>
+        ));
+
+        const originArray = originsCityArray.map((city, index) => (
+            <option value={this.state.origins[city]} key={`${city}-${index}`}>
+                {city}
+            </option>
+        ));
+
+        return (
+            <div className="App">
+                <div className="app-wrapper">
+                    <div>
+                        <h1>Search for flights</h1>
+                        {this.state.isLoading && (
+                            <img src={Gif} alt="spinner" />
+                        )}
+                    </div>
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor="origin">From</label> <br />
+                        <select id="origin" onChange={this.handleOriginSelect}>
+                            {originArray}
+                        </select>{' '}
+                        <br />
+                        <label htmlFor="destination">To</label> <br />
+                        <select
+                            id="destination"
+                            onChange={this.handleDestSelect}
+                        >
+                            {destArray}
+                        </select>{' '}
+                        <br />
+                        <button onClick={this.fetchFlights}>Search</button>{' '}
+                        <br />
+                        <label>
+                            Direct only
+                            <input
+                                name="isDirect"
+                                type="checkbox"
+                                checked={this.state.isDirect}
+                                onChange={this.handleInputChange}
+                            />
+                        </label>
+                    </form>
+                    <div
+                        style={{
+                            marginTop: `200px`
+                        }}
+                    >
+                        {flightComponents}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
