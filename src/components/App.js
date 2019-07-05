8bbd6acf82d25dd170f5528f69ab99c48be6306b
Marijan3062 <brailo.marijan@gmail.com>
2019-07-05 15:41:23 +0200

    Added number of transfers

--- src/components/App.js
+++ src/components/App.js
@@ -97,6 +97,7 @@ class App extends React.Component {
                     price={flight.price}
                     dTime={depTime}
                     aTime={arrTime}
+                    transfers={flight.transfers}
                 />
             );
         });
--- src/components/Flight.js
+++ src/components/Flight.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React from 'react';
 
 const Flight = props => {
-  return (
-    <div className="flight-wrapper">
-      <span>
-        {props.cityFrom} ({props.flyFrom})
-      </span>
-      <span>
-        {props.cityTo} ({props.flyTo})
-      </span>
-      <span>€{props.price}</span>
-      <span>{props.dTime}</span>
-      <span>{props.aTime}</span>
-    </div>
-  );
+    return (
+        <div className="flight-wrapper">
+            <span>
+                {props.cityFrom} ({props.flyFrom})
+            </span>
+            <span>
+                {props.cityTo} ({props.flyTo})
+            </span>
+            <span>€{props.price}</span>
+            <span>{props.dTime}</span>
+            <span>{props.aTime}</span>
+            <span>Number of transfers: {props.transfers.length}</span>
+        </div>
+    );
 };
 
 export default Flight;
