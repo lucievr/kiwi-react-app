0b8d0472af366dc7c7b714030689b86bc56f6242
Lucie <46979603+lucievr@users.noreply.github.com>
2019-07-05 11:27:33 +0200

    Add loading logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import axios from 'axios';
-import Flight from './Flight';
-import { DateTime } from 'luxon';
+import axios from "axios";
+import Flight from "./Flight";
+import { DateTime } from "luxon";
+import Gif from "../img/spinner.gif";
 
 class App extends React.Component {
   constructor(props) {
@@ -9,43 +10,43 @@ class App extends React.Component {
     this.state = {
       flights: [],
       isLoading: true
-    }
+    };
   }
 
   fetchFlights = async () => {
-
     try {
-      let response = await axios.get('https://api.skypicker.com/flights', {
+      let response = await axios.get("https://api.skypicker.com/flights", {
         params: {
-          fly_from: 'PRG',
-          fly_to: 'VLC',
-          date_from: '06/07/2019',
-          date_to: '13/07/2019',
+          fly_from: "PRG",
+          fly_to: "VLC",
+          date_from: "06/07/2019",
+          date_to: "13/07/2019",
           limit: 15
         }
-      })
-
-    let { data } = response.data
-    this.setState({flights: data})
-      console.log(this.state.flights)
-  } catch (error) {
-    console.error(error);
-  }
-  }
+      });
+      let { data } = response.data;
+      await this.setState({ flights: data });
+      this.setState({ isLoading: false });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   componentDidMount() {
-    this.fetchFlights()
+    this.fetchFlights();
   }
 
   render() {
-
-
     const flightComponents = this.state.flights.map((flight, index) => {
-      const depTime = DateTime.fromMillis(flight.dTime * 1000).toFormat('hh:mm');
-      const arrTime = DateTime.fromMillis(flight.aTime * 1000).toFormat('hh:mm');
-        return (
-          <Flight 
-          key={`flight-${index}`} 
+      const depTime = DateTime.fromMillis(flight.dTime * 1000).toFormat(
+        "hh:mm"
+      );
+      const arrTime = DateTime.fromMillis(flight.aTime * 1000).toFormat(
+        "hh:mm"
+      );
+      return (
+        <Flight
+          key={`flight-${index}`}
           cityFrom={flight.cityFrom}
           cityTo={flight.cityTo}
           flyFrom={flight.flyFrom}
@@ -53,17 +54,18 @@ class App extends React.Component {
           price={flight.price}
           dTime={depTime}
           aTime={arrTime}
-          />
-        )
-    })
+        />
+      );
+    });
 
     return (
       <div className="App">
         <div className="app-wrapper">
-          <header>
+          <div>
             <h1>Kiwi Flights</h1>
-          </header>
-          <div>{ flightComponents }</div>
+            {this.state.isLoading && <img src={Gif} alt="spinner" />}
+          </div>
+          <div>{flightComponents}</div>
         </div>
       </div>
     );
